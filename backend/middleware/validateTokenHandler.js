c71a53dc3669ee429b2bf19ca43bab20d36b2191
szymon-skalmierski <xketris@gmail.com>
2025-07-23 00:25:16 +0200

    jwt.validate instead of decode

--- backend/middleware/validateTokenHandler.js
+++ backend/middleware/validateTokenHandler.js
@@ -6,28 +6,34 @@ const validateToken = asyncHandler( async (req, res, next) => {
     if(accessToken) {
         jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, {
             ignoreExpiration: true
-        }, (err, decoded) => {
+        }, (err, decodedAccessToken) => {
             if(err) {
                 res.status(401);
                 throw new Error("User is not authorized");
             }
 
             const dateNow = Math.round(Date.now()/1000);
-            if(decoded.exp < dateNow) {
-                const decodedRefreshToken = jwt.decode(req.cookies.refreshToken, process.env.REFRESH_TOKEN_SECRET);
-                if(decoded.session_id === decodedRefreshToken.session_id && decodedRefreshToken.exp > dateNow) {
-                    console.log("new access token generated!")
-                    res.cookie("accessToken", jwt.sign({ user: decoded.user, session_id: decoded.session_id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10s" }), {overwrite: true});
-                } else {
-                    console.log("invalid refresh token!")
-                    res.clearCookie("accessToken");
-                    res.clearCookie("refreshToken");
-                    res.status(401);
-                    throw new Error("User is not authorized");
-                }
+            if(decodedAccessToken.exp < dateNow) {
+                jwt.verify(req.cookies.refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decodedRefreshToken) => {
+                    if(err) {
+                        res.status(401);
+                        throw new Error("User is not authorized");
+                    }
+
+                    if(decodedAccessToken.session_id === decodedRefreshToken.session_id && decodedRefreshToken.exp > dateNow) {
+                        console.log("new access token generated!")
+                        res.cookie("accessToken", jwt.sign({ user: decodedAccessToken.user, session_id: decodedAccessToken.session_id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10s" }), {overwrite: true});
+                    } else {
+                        console.log("invalid refresh token!")
+                        res.clearCookie("accessToken");
+                        res.clearCookie("refreshToken");
+                        res.status(401);
+                        throw new Error("User is not authorized");
+                    }
+                });
             }
 
-            req.user = decoded.user;
+            req.user = decodedAccessToken.user;
             next();
         })
     } else {
