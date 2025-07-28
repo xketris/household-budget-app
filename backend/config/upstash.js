2c155a9fe60e54be1580aa55ecd353c142b41f20
xketris <xketris@gmail.com>
2025-07-28 05:01:08 +0200

    rateLimiter

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -2,9 +2,9 @@ import { Redis } from '@upstash/redis'
 import { Ratelimit } from '@upstash/ratelimit';
 import "dotenv/config";
 
-const ratelimiter = new Ratelimit({
+const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     limiter: Ratelimit.slidingWindow(100, "60s")
 });
 
-export default ratelimiter;
+export default ratelimit;
---
+++ backend/middleware/rateLimiter.js
@@ -0,0 +1,18 @@
+import ratelimit from "../config/upstash.js"
+
+const rateLimiter = async (req, res, next) => {
+    try {
+        const { success } = await ratelimit.limit(req.user.id);
+        if(!success) {
+            res.status(429);
+            throw new Error("Too many request, try again later")
+        }
+        next();
+    } catch(err) {
+        console.log("Rate limit error", err);
+        res.status(401);
+        throw new Error("Rate limit error")
+    }
+}
+
+export default rateLimiter;
