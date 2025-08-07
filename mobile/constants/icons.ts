98b2e176221a64c4582f497c6bc78dc11c9c5be7
szymon-skalmierski <xketris@gmail.com>
2025-08-07 05:11:51 +0200

    icons

---
+++ mobile/constants/icons.ts
@@ -0,0 +1,11 @@
+import home from "../assets/icons/home.png"
+import transaction from "../assets/icons/transaction.png"
+import report from "../assets/icons/report.png"
+import profile from "../assets/icons/profile.png"
+
+export const icons = {
+    home,
+    transaction,
+    report,
+    profile
+}
