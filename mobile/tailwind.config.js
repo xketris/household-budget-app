36deff1b30abcc228ce90d6c1c8ef05c899f03f8
szymon-skalmierski <xketris@gmail.com>
2025-08-07 02:33:23 +0200

    nativewind colors preset

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#121212",
+        foreground: "#e0e0e0",
+        primary: "#000000",
+        secondary: "#ab9249",
+        border: "#333333",
+        input: "#1a1a1a"
+      }
+    },
   },
   plugins: [],
 }
