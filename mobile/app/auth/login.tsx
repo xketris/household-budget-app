63bb51dd63fb41cb1e8d69bcba1f1c361f257a71
szymon-skalmierski <xketris@gmail.com>
2025-08-08 14:19:28 +0200

    login, register pages

--- mobile/app/auth/login.tsx
+++ mobile/app/auth/login.tsx
@@ -9,8 +9,8 @@ const LoginPage = () => {
     <View className='bg-background flex-1 h-full w-full flex-col justify-center items-center px-16'>
         <Text className='text-foreground text-5xl font-bold pb-12'>Welcome Back</Text>
         <View className='flex-col w-full justify-around items-center gap-5'>
-            <CustomInput placeholder="Login" />
-            <CustomInput placeholder="Password" secureTextEntry={true} />
+            <CustomInput placeholder="Login" className="rounded-3xl" />
+            <CustomInput placeholder="Password" className="rounded-3xl" secureTextEntry />
             <TouchableOpacity className='bg-primary w-full py-5 rounded-full'>
                 <Text className='text-background font-bold text-center text-2xl'>Login</Text>
             </TouchableOpacity>
--- mobile/app/auth/register.tsx
+++ mobile/app/auth/register.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import CustomInput from '@/components/CustomInput'
 
 const RegisterPage = () => {
   return (
-    <View>
-      <Text>RegisterPage</Text>
+    <View className='bg-background flex-1 h-full w-full flex-col justify-center items-center px-16'>
+        <Text className='text-foreground text-5xl font-bold pb-12'>Create Account</Text>
+        <View className='flex-col w-full justify-around items-center gap-5'>
+            <View className='flex-row justify-between space-x-2'>
+                <CustomInput placeholder="First Name" className="flex-1 mr-3" />
+                <CustomInput placeholder="Last Name" className="flex-1 ml-3" />
+            </View>
+            <CustomInput placeholder="Email" className="w-full" />
+            <CustomInput placeholder="Password" className="w-full" secureTextEntry />
+            <CustomInput placeholder="Repeat Password" className="w-full" secureTextEntry />
+            <TouchableOpacity className='bg-primary w-full py-5 rounded-3xl'>
+                <Text className='text-background font-bold text-center text-2xl'>Login</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   )
 }
