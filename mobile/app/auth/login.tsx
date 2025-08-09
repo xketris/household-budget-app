1836391a5a0e0663ca7e9ef989dfa76fd09d2ddc
szymon-skalmierski <xketris@gmail.com>
2025-08-09 10:43:47 +0200

    inputs improved

--- mobile/app/auth/login.tsx
+++ mobile/app/auth/login.tsx
@@ -8,10 +8,10 @@ const LoginPage = () => {
   return (
     <View className='bg-background flex-1 h-full w-full flex-col justify-center items-center px-16'>
         <Text className='text-foreground text-5xl font-bold pb-12'>Welcome Back</Text>
-        <View className='flex-col w-full justify-around items-center gap-5'>
+        <View className='flex-col w-full justify-around gap-5'>
             <CustomInput placeholder="Login" className="rounded-3xl" />
             <CustomInput placeholder="Password" className="rounded-3xl" secureTextEntry />
-            <TouchableOpacity className='bg-primary w-full py-5 rounded-full'>
+            <TouchableOpacity className='bg-primary w-full py-5 rounded-3xl'>
                 <Text className='text-background font-bold text-center text-2xl'>Login</Text>
             </TouchableOpacity>
         </View>
