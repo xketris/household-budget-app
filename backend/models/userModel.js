171f3ade085c353e9b5ba0cc57beb6ae6a5f18ce
szymon-skalmierski <xketris@gmail.com>
2025-10-27 17:06:37 +0100

    user model email required

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -11,7 +11,8 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        required: [ true, "Email is required" ]
+        required: [ true, "Email is required" ],
+        unique: true
     },
     password: {
         type: String,
